c88e801ce87ca9e495d288b76098f1473fc3bd94
cjmcd123 <cj_mcdowall@btinternet.com>
2018-06-20 16:18:08 +0100

    error correction

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Deck {
 
-    private ArrayList<Card> cards = new ArrayList<Card>();
+    private ArrayList<Card> cards;
 
     public Deck(){
         cards = new ArrayList<Card>();
